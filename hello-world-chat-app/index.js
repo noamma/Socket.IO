6dacfbbb83019061fe5bebfa2e7db5693d6fde16
root <noamma@nvgnet.co.il>
2017-11-08 18:24:38 +0200

    chat faze 1

--- hello-world-chat-app/index.js
+++ hello-world-chat-app/index.js
@@ -6,6 +6,12 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+    });
+});
+
 http.listen(3000, function(){
     console.log('listening on *:3000');
 });
